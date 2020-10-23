8dbc11dfc6e373f39bb2bfd066d8f1336b1cc521
josDugan <josephdug@gmail.com>
2020-10-22 20:02:17 -0400

    Initial commit. Closes #1.

---
+++ src/test/java/guru/springframework/mssc/brewery/config/MsscBreweryConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.brewery.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
